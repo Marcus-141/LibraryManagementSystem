cba95a300e3447d162ce771abaffb2d009f0f072
TanZhengYang <alexstein1663@gmail.com>
2024-08-17 23:00:11 +0800

    Commiting new file

---
+++ src/main/scala/ch/makery/addres/model/Library.Scala
@@ -0,0 +1,10 @@
+package ch.makery.address
+import ch.makery.address.model.Person
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes._
+import scalafxml.core.{FXMLLoader, FXMLView, NoDependencyResolver}
+import javafx.{scene => jfxs}
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
